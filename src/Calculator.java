d570a45bb0b60e92d859e654a604257c0745fca3
dev_flo <florianemonnini@gmail.com>
2025-01-27 11:59:08 +0100

    final commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,29 +1,62 @@
 public class Calculator {
-    private double firstOperation = 0.0;
+    private String affichNbr       = "0";
+    private double firstOperation  = 0.0;
     private double secondOperation = 0.0;
+    public String operation        = null;
 
     public double addition(double firstOperation, double secondOperation) {
         var addiction = firstOperation + secondOperation;
-        return addiction;
+        return Math.round(addiction);
     }
 
     public double soustrac(double firstOperation, double secondOperation) {
         var soustrac = firstOperation - secondOperation;
-        return soustrac;
+        return Math.round(soustrac);
     }
 
     public double multi(double firstOperation, double secondOperation) {
         var multi = firstOperation * secondOperation;
-        return multi;
+        return Math.round(multi);
     }
 
     public double divise(double firstOperation, double secondOperation) {
         var divis = firstOperation / secondOperation;
-        return divis;
+        //Gestion des erreurs
+        if (firstOperation == 0 && secondOperation == 0) {
+            System.out.println("is NaN");
+        } else {
+            return Math.round(divis);
+        }
+        return Math.round(divis);
     }
 
     public double modulo(double firstOperation, double secondOperation) {
         var modulo = firstOperation % secondOperation;
-        return modulo;
+        //Gestion des erreurs
+        if (firstOperation == 0 && secondOperation == 0) {
+            System.out.println("is NaN");
+        } else {
+            return Math.round(modulo);
+        }
+
+        return Math.round(modulo);
+    }
+
+    // Calcule 2 nombres avec divers opérateurs tant qu'une opération est effectuée
+    public String isEqual(String operation) {
+        if (operation == "+") {
+            affichNbr = String.valueOf(addition(firstOperation, secondOperation));
+        } else if (operation == "-") {
+            affichNbr = String.valueOf(soustrac(firstOperation, secondOperation));
+        } else if (operation == "*") {
+            affichNbr = String.valueOf(soustrac(firstOperation, secondOperation));
+        } else if (operation == "/") {
+            affichNbr = String.valueOf(divise(firstOperation, secondOperation));
+        } else if (operation == "%") {
+            affichNbr = String.valueOf(modulo(firstOperation, secondOperation));
+        } else {
+            System.out.println("Error");
+        }
+        return operation;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.addition(10, 20));
-        System.out.println(calculator.soustrac(50, 10));
-        System.out.println(calculator.multi(8, 2));
-        System.out.println(calculator.divise(4, 2));
-        System.out.println(calculator.modulo(5, 2));
+
+        //Calcul de bases
+        System.out.println("Calcul de base");
+        System.out.println("10 + 20 = " + calculator.addition(10,20));
+        System.out.println("50 - 10 = " +  calculator.soustrac(50, 10));
+        System.out.println("8 * 2   = " + calculator.multi(8,2));
+        System.out.println("4 / 2   = " + calculator.divise(4,2));
+        System.out.println("5 % 2   = " + calculator.modulo(5,2));
+
+        //essais avec plusieurs calculs
+        System.out.println("\nCalcul complexe");
+        System.out.println(calculator.soustrac(5, 2) + " " + calculator.isEqual("+") + " " + calculator.addition(30, 4444));
+        System.out.println(calculator.divise(5, 2) + calculator.isEqual("-") + calculator.multi(1, 1));
+        System.out.println(calculator.isEqual("-"));
+
+        //test de division par zero
+        System.out.println("\nDivision par zéro");
+        System.out.println("0 / 0 = " + calculator.divise(0,0));
+        System.out.println("0 % 0 = " + calculator.modulo(0,0));
     }
 }
